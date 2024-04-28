8069c851886d01401282582691b4b53ce298fcd4
amrandev <amrit.randev@sjsu.edu>
2024-04-28 13:51:49 -0700

    updated resource component

--- src/pages/components/resources.tsx
+++ src/pages/components/resources.tsx
@@ -6,20 +6,41 @@ const Resource = () => {
             <div className="crime-box">
                 <h2>Assault</h2>
                 <p>Get some resources for Assault</p>
-                <p>https://www.libertymissouri.gov/2594/Robbery-Prevention-Tips</p>
-                <p>National Crime Prevention Council (NCPC)</p>
+                <p>Organizations: National Organization for Victim Assistance (NOVA)</p>
+                <p>Resources: https://www.justice.gov/enrd/environmental-crime-victim-assistance/assistance-and-resources-victims</p>
             </div>
             <div className="crime-box">
                 <h2>Robbery</h2>
                 <p>Get some resources for Robbery </p>
-                <p>https://www.libertymissouri.gov/2594/Robbery-Prevention-Tips</p>
-                <p>National Crime Prevention Council (NCPC)</p>
+                <p>Organizations: National Crime Prevention Council (NCPC)</p>
+                <p>Resources: https://www.libertymissouri.gov/2594/Robbery-Prevention-Tips</p>
             </div>
             <div className="crime-box">
-                <h2>Rape</h2>
-                <p>Information about rape...</p>
+                <h2>Property Crime</h2>
+                <p>Get some resources for Property Crimes</p>
+                <p>Organizations: National Association of Town Watch (NATW)</p>
+                <p>Resources: https://www.pvestates.org/services/burglary-prevention</p>
             </div>
-
+            <div className="crime-box">
+                <h2>Burglary</h2>
+                <p>Get some resources for Burglary</p>
+                <p>Organizations: National Burglar & Fire Alarm Association (NBFAA)</p>
+                <p>Resources: https://www.sjpd.org/community/crime-prevention/prevention-tips/burglary-prevention</p>
+            </div>
+            <div className="crime-box">
+                <h2>Larceny Theft</h2>
+                <p>Get some resources for Larceny Theft </p>
+                <p>Organizations: National Association of Shoplifting Prevention (NASP)</p>
+                <p>Resources: https://www.salisbury.edu/police/crime-prevention/personal-safety-tips.aspx#:~:text=Keep%20your%20door%20locked%20at,where%20someone%20could%20be%20hiding.</p>
+            </div>
+            <div className="crime-box">
+                <h2>Motor Vehicle Theft</h2>
+                <p>Get some resources for Motor Vehicle Theft</p>
+                <p>Organizations: National Insurance Crime Bureau (NICB)</p>
+                <p>Resources: https://www.nhtsa.gov/vehicle-safety/vehicle-theft-prevention</p>
+            </div>
+            
+            
         </div>
 
     
