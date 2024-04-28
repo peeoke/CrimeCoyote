3b2ef93f2fe251b1099c952f9a8f76977daffeed
juliahusainzada <juliahusainzada@gmail.com>
2024-04-28 02:40:12 -0700

    Added pie chart data visualization using plotly.express

--- server/data.py
+++ server/data.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-
+import plotly.express as px
 
 crime_df = pd.read_csv('/Users/julzhusainzada/Desktop/california.csv')
 
@@ -17,5 +16,24 @@ crime_df.columns = ['City', 'Population', 'Violent crime', 'Murder and nonneglig
 
 city = str(input("Enter a city: "))
 city_row_index = crime_df[crime_df['City'] == city].index.item()
+
+# Function to generate interactive pie chart for a given city
+def generate_pie_chart(city):
+    selected_city = crime_df[crime_df['City'] == city]
+    population = selected_city['Population'].values[0]  # Extract population number
+    crime_labels = selected_city.columns[2:]
+    crime_values_str = selected_city.values[0][2:]
+
+    crime_values = [int(value.replace(',', '')) for value in crime_values_str]
+
+    custom_colors = ['#89CFF0', '#B0E0E6', '#87CEEB', '#F0F8FF', '#CCCCFF', '#e5f2ff', '#81D8D0', '#00CCCC', '#AFEEEE', '#B2F0E4', '#ADD8E6']
+
+    fig = px.pie(values=crime_values, names=crime_labels, title=f'<b>Crime Distribution in {city}<br><span style="font-size: 18px;">Population: {population}</span></b>',
+                 hover_name=crime_labels, labels={'label': 'Crime'}, hole=0.3, color_discrete_sequence=custom_colors)
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig.update_layout(showlegend=False, title_x=0.5, title_font_size=24)  # Increase title font size
+
+    fig.show()
+
 
 
