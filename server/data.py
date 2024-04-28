9577d4f7d0c8858a56d0495a9ded48a1618830d4
juliahusainzada <juliahusainzada@gmail.com>
2024-04-27 22:06:21 -0700

    Changed dataframe name from 'data' to 'crime_df'

--- server/data.py
+++ server/data.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pd.read_csv('/Users/julzhusainzada/Desktop/california.csv')
+
+crime_df = pd.read_csv('/Users/julzhusainzada/Desktop/california.csv')
 
 # Dropping first three rows bc no data in them
-data = data.drop([0, 1, 2, 3])
+crime_df = crime_df.drop([0, 1, 2, 3])
 
 # Reset the index after dropping rows
-data.reset_index(drop=True, inplace=True)
+crime_df.reset_index(drop=True, inplace=True)
 
 # Reassigning column names because originally unnamed
-data.columns = ['City', 'Population', 'Violent crime', 'Murder and nonnegligent manslaughter',
+crime_df.columns = ['City', 'Population', 'Violent crime', 'Murder and nonnegligent manslaughter',
                 'Rape', 'Robbery', 'Aggravated assault', 'Property Crime', 'Burglary',
                 'Larcenytheft', 'Motor vehicle theft', 'Arson']
 
 city = str(input("Enter a city: "))
-city_row_index = data[data['City'] == city].index.item()
+city_row_index = crime_df[crime_df['City'] == city].index.item()
+
+
